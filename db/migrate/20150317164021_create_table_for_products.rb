9d512ffacce4eb9b28af6460214813bde766bd07
Jason Hogrefe <hogrefejason@gmail.com>
2015-03-17 11:43:25 -0500

    Added migration to create products table.

---
+++ db/migrate/20150317164021_create_table_for_products.rb
@@ -0,0 +1,9 @@
+class CreateTableForProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      
+      t.timestamps
+  end
+end
