7303a3a85989650181699361d1901f1150a9d29c
Jason Hogrefe <hogrefejason@gmail.com>
2015-03-17 11:22:52 -0500

    First commit. Created new Rails example. Removed username and password from database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyProduct::Application.config.secret_token = '86c85302cc20653ef47aabb657c5da4a8431ab8076901a18e156acd4a3ed18b7b0e0ac329631c479980b7083557974a90a2507140f93b74bc7a049a029ef868c'
